a0b86e4e1702c2436e1ed5cc35c6d160c483a0fd
Strom-Cuzewon <mbackes5052@gmail.com>
2024-04-05 22:08:04 -0500

    Added tests with references.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,18 @@ fn main() {
     let s2 = takes_and_gives(s2);
 
     println!("{}", s2);
+
+    let s3 = String::from("Testing references");
+
+    let len = calc_len(&s3);
+
+    println!("{} length: {}", s3, len); //S3 string was use in calc_len function, but is still valid, unlike S used above.
+
+    let mut s4 = String::from("Testing mutable reference."); //Because s4 is mutable, only one reference to it can exist at any given time.
+
+    println!("{}", s4);
+    change(&mut s4);
+    println!("{}", s4);
 }
 
 fn takes_ownership(some: String) {
@@ -17,4 +29,12 @@ fn takes_ownership(some: String) {
 fn takes_and_gives(some: String) -> String {
     println!("{}", some);
     String::from("Testing return.")
+}
+
+fn calc_len(some: &String) -> usize {
+    some.len() // Attempting to modify "Some" will cause an error because the reference is immutable;
+}
+
+fn change(some: &mut String) {
+    some.push_str(" ADDED TEXT"); //Reference was passed as immutable, no error will occur here.
 }
